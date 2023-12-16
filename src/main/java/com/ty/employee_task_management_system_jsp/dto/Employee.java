a29fa95d9b4492557a292fe4c8192cc710d8c9a4
Raki0110 <rakeshgowdavviet2000@gmail.com>
2023-12-16 15:40:21 +0530

    daolayercreated

--- src/main/java/com/ty/employee_task_management_system_jsp/dto/Employee.java
+++ src/main/java/com/ty/employee_task_management_system_jsp/dto/Employee.java
@@ -1,5 +1,7 @@
 package com.ty.employee_task_management_system_jsp.dto;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +12,55 @@ public class Employee {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	int id;
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+	private double salary;
+	private List<Task> task;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public List<Task> getTask() {
+		return task;
+	}
+	public void setTask(List<Task> task) {
+		this.task = task;
+	}
 
 }
--- src/main/java/com/ty/employee_task_management_system_jsp/dto/Task.java
+++ src/main/java/com/ty/employee_task_management_system_jsp/dto/Task.java
@@ -10,8 +10,22 @@ public class Task {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	int id;
-	String description;
+	private int id;
+	private String description;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	
 	
 
